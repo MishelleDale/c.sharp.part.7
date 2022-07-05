c62c1df65a7bb8f3428ed2b9861d4de82dc6910f
Ekaterina <mishelledale@gmail.com>
2022-07-05 18:34:50 +0300

    Task1: Sort the elements of a row from max to min is done

---
+++ Task1/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задайте двумерный массив. 
+// Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int [,] CreateArray (int n, int m)
+{
+    return new int [n, m];
+}
+
+void FillArray (int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1,10);
+        }
+    }
+}
+
+void PrintArray (int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}  ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void ChangeArray(int [,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+Console.Write("Введите количество строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] arr = CreateArray(row,column);
+FillArray(arr);
+PrintArray(arr);
+ChangeArray(arr);
+Console.WriteLine();
+PrintArray(arr);
